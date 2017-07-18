1c32946a2af65ae4c53342e77e7c8992943e87d5
Chan Young Park <chpark@ml-t0001249.ngs.org>
2017-07-18 17:16:19 -0400

    added basic

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ if (document.readyState !== 'loading') init();
 else document.addEventListener('DOMContentLoaded', init);
 
 function init(){
+	const $$dataset = document.querySelectorAll('script[type="text/news-chart"]');
 
+	for (let i = 0; i < $$dataset.length; i++) {
+
+		const $data = $$dataset[i];
+		const $wrp = $data.parentNode;
+
+	}
 }
